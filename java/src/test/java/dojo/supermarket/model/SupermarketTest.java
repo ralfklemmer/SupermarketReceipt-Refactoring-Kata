f714c4d648f593cf1ed7e8b99075133b5c2141cd
Ralf Klemmer <r_klemmer@gmx.de>
2022-02-24 17:58:50 +0100

    Added Approval Tests

--- java/src/test/java/dojo/supermarket/model/SupermarketTest.java
+++ java/src/test/java/dojo/supermarket/model/SupermarketTest.java
@@ -30,16 +30,78 @@ public class SupermarketTest {
         Receipt receipt = teller.checksOutArticlesFrom(cart);
 
         // ASSERT
-        assertEquals(4.975, receipt.getTotalPrice(), 0.01);
-        assertEquals(Collections.emptyList(), receipt.getDiscounts());
-        assertEquals(1, receipt.getItems().size());
-        ReceiptItem receiptItem = receipt.getItems().get(0);
-        assertEquals(apples, receiptItem.getProduct());
-        assertEquals(1.99, receiptItem.getPrice());
-        assertEquals(2.5*1.99, receiptItem.getTotalPrice());
-        assertEquals(2.5, receiptItem.getQuantity());
+        ReceiptPrinter printer = new ReceiptPrinter();
+        Approvals.verify(printer.printReceipt(receipt));
+    }
 
+    @Test
+    public void threeForTwo() {
+        SupermarketCatalog catalog = new FakeCatalog();
+        Product apples = new Product("apples", ProductUnit.Kilo);
+        catalog.addProduct(apples, 1.99);
+
+        Teller teller = new Teller(catalog);
+        teller.addSpecialOffer(SpecialOfferType.ThreeForTwo, apples, 10.0);
+
+        ShoppingCart cart = new ShoppingCart();
+        cart.addItemQuantity(apples, 2.5);
+        cart.addItemQuantity(apples, 2.5);
+        cart.addItemQuantity(apples, 2.5);
+
+        // ACT
+        Receipt receipt = teller.checksOutArticlesFrom(cart);
+
+        // ASSERT
+        ReceiptPrinter printer = new ReceiptPrinter();
+        Approvals.verify(printer.printReceipt(receipt));
     }
 
 
+    @Test
+    public void twoForPrice() {
+        SupermarketCatalog catalog = new FakeCatalog();
+        Product toothbrush = new Product("toothbrush", ProductUnit.Each);
+        catalog.addProduct(toothbrush, 0.99);
+        Product apples = new Product("apples", ProductUnit.Kilo);
+        catalog.addProduct(apples, 1.99);
+
+        Teller teller = new Teller(catalog);
+        teller.addSpecialOffer(SpecialOfferType.TwoForAmount, apples, 1.00);
+        teller.addSpecialOffer(SpecialOfferType.TwoForAmount, toothbrush, 0.75);
+
+        ShoppingCart cart = new ShoppingCart();
+        cart.addItemQuantity(toothbrush, 2);
+        cart.addItemQuantity(apples, 3);
+
+        // ACT
+        Receipt receipt = teller.checksOutArticlesFrom(cart);
+
+        // ASSERT
+        ReceiptPrinter printer = new ReceiptPrinter();
+        Approvals.verify(printer.printReceipt(receipt));
+    }
+
+
+    @Test
+    public void fiveForAmount() {
+        SupermarketCatalog catalog = new FakeCatalog();
+        Product toothbrush = new Product("toothbrush", ProductUnit.Each);
+        catalog.addProduct(toothbrush, 0.99);
+        Product apples = new Product("apples", ProductUnit.Kilo);
+        catalog.addProduct(apples, 1.99);
+
+        Teller teller = new Teller(catalog);
+        teller.addSpecialOffer(SpecialOfferType.FiveForAmount, apples, 1.00);
+
+        ShoppingCart cart = new ShoppingCart();
+        cart.addItemQuantity(toothbrush, 2);
+        cart.addItemQuantity(apples, 6);
+
+        // ACT
+        Receipt receipt = teller.checksOutArticlesFrom(cart);
+
+        // ASSERT
+        ReceiptPrinter printer = new ReceiptPrinter();
+        Approvals.verify(printer.printReceipt(receipt));
+    }
 }
